564b49736203741bac14dd079796dca8b545fb51
Your Name <you@example.com>
2022-12-27 18:00:49 +0100

    22

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2852,9 +2852,9 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
+    primary: "#4A9633",
+    primaryBright: "#4A9633",
+    primaryDark: "#4A9633",
     secondary: "#7645D9",
     success: "#31D0AA",
     warning: "#FFB237",
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,9 +2,9 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#aa8929",
-  primaryBright: "#aa8929",
-  primaryDark: "#aa8929",
+  primary: "#4A9633",
+  primaryBright: "#4A9633",
+  primaryDark: "#4A9633",
   secondary: "#7645D9",
   success: "#31D0AA",
   warning: "#FFB237",
